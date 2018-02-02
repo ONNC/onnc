038a8f765b91963df75b5e154f2eb2518d91cbc6
Luba tang <luba@skymizer.com>
2018-02-02 13:31:47 +0800

    Add class onnc::sophon::OutputFormat.

---
+++ lib/Target/Sophon/ObjectFormat.h
@@ -0,0 +1,26 @@
+//===- ObjectFormat.h -----------------------------------------------------===//
+//
+//                             The ONNC Project
+//
+// See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+#ifndef TARGET_SOPHON_OBJECT_FORMAT_H
+#define TARGET_SOPHON_OBJECT_FORMAT_H
+#include <string>
+
+namespace onnc {
+namespace sophon {
+
+/** \class ObjectFormat
+ *  \brief ObjectFormat provides interface for an output file of sophon onnc
+ *  compiler.
+ */
+class ObjectFormat
+{
+};
+
+} // namespace of sophon
+} // namespace of onnc
+
+#endif  // TARGET_SOPHON_SOPHON_H
