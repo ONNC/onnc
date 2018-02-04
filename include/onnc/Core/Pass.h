13406b1af970f0d2afd6278ea900fcdca135faac
Luba tang <luba@skymizer.com>
2018-02-04 21:18:13 +0800

    Add files Pass.h and PassManager.h.

---
+++ include/onnc/Core/Pass.h
@@ -0,0 +1,22 @@
+//===- Pass.h -------------------------------------------------------------===//
+//
+//                             The ONNC Project
+//
+// See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+#ifndef ONNC_CORE_PASS_H
+#define ONNC_CORE_PASS_H
+
+namespace onnc {
+
+/** \class onnc::Pass
+ *  \brief encapsulate transformation algorithms.
+ */
+class Pass
+{
+};
+
+} // namespace of onnc
+
+#endif
---
+++ include/onnc/Core/PassManager.h
@@ -0,0 +1,22 @@
+//===- PassManager.h -----------------------------------------------------===//
+//
+//                             The ONNC Project
+//
+// See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+#ifndef ONNC_CORE_PASS_MANAGER_H
+#define ONNC_CORE_PASS_MANAGER_H
+
+namespace onnc {
+
+/** \class onnc::PassManager
+ *  \brief stores a set of passes and run them.
+ */
+class PassManager
+{
+};
+
+} // namespace of onnc
+
+#endif
