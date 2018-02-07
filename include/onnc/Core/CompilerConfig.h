d3fc9255ada40d96b4c07e4a31dd0699d045979f
Luba tang <luba@skymizer.com>
2018-02-07 12:57:01 +0800

    Add scaffolding of CompilerConfig.

---
+++ include/onnc/Core/CompilerConfig.h
@@ -0,0 +1,23 @@
+//===- CompilerConfig.h ---------------------------------------------------===//
+//
+//                             The ONNC Project
+//
+// See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+#ifndef ONNC_CORE_COMPILER_CONFIG_H
+#define ONNC_CORE_COMPILER_CONFIG_H
+
+namespace onnc {
+
+/** \class CompilerConfig
+ *  \brief CompilerConfig stores settings of a compiler
+ *  XXX: should it be in Target/ or Core/?
+ */
+class CompilerConfig
+{
+};
+
+} // namespace of onnc
+
+#endif
