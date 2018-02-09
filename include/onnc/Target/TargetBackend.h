ca08384d3a14ac98b7e70498ff953b64bad905d5
Luba tang <luba@skymizer.com>
2018-02-09 17:02:59 +0800

    Add three basic lowering methods.

--- include/onnc/Target/TargetBackend.h
+++ include/onnc/Target/TargetBackend.h
@@ -7,6 +7,7 @@
 //===----------------------------------------------------------------------===//
 #ifndef ONNC_TARGET_TARGET_BACKEND_H
 #define ONNC_TARGET_TARGET_BACKEND_H
+#include <onnc/Core/PassManager.h>
 
 namespace onnc {
 
@@ -18,6 +19,12 @@ public:
   TargetBackend(const CompilerConfig& pConfig);
 
   virtual ~TargetBackend();
+
+  virtual void addTensorSel(PassManager& pPM) { return; }
+
+  virtual void addMemAlloc(PassManager& pPM) { return; }
+
+  virtual void addCodeEmit(PassManager& pPM) { return; }
 };
 
 } // namespace of onnc
