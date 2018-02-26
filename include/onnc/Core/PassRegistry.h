51f7b0396d324d58fa0ca41d2fe5abe163260a43
Luba tang <luba@skymizer.com>
2018-02-26 14:49:55 +0800

    Add scaffolding of class PassRegistry.
    PassRegistry bookkeeps all creatable passes.

---
+++ include/onnc/Core/PassRegistry.h
@@ -0,0 +1,22 @@
+//===- PassRegistry.h -----------------------------------------------------===//
+//
+//                             The ONNC Project
+//
+// See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+#ifndef ONNC_CORE_PASS_REGISTRY_H
+#define ONNC_CORE_PASS_REGISTRY_H
+
+namespace onnc {
+
+/** \class onnc::PassRegistry
+ *  \brief stores a set of pass ID and the 
+ */
+class PassRegistry
+{
+};
+
+} // namespace of onnc
+
+#endif
