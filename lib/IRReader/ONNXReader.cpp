03ddaf7ad6f6ef01eaae6d41b58fbe9d95437e9b
Luba tang <luba@skymizer.com>
2018-03-05 01:26:13 +0800

    Initialize/Finalize Google protocol buffer library.

--- lib/IRReader/ONNXReader.cpp
+++ lib/IRReader/ONNXReader.cpp
@@ -6,6 +6,8 @@
 //
 //===----------------------------------------------------------------------===//
 #include <onnc/IRReader/ONNXReader.h>
+#include <google/protobuf/io/coded_stream.h>
+#include <google/protobuf/io/zero_copy_stream_impl.h>
 
 using namespace onnc;
 
@@ -14,10 +16,15 @@ using namespace onnc;
 //===----------------------------------------------------------------------===//
 onnx::Reader::Reader()
 {
+  // Verify that the version of the library that we linked against is
+  // compatible with the version of the headers we compiled against.
+  GOOGLE_PROTOBUF_VERIFY_VERSION;
 }
 
 onnx::Reader::~Reader()
 {
+  // Optional:  Delete all global objects allocated by libprotobuf.
+  google::protobuf::ShutdownProtobufLibrary();
 }
 
 SystemError onnx::Reader::parse(const Path& pFileName, Module& pModule)
