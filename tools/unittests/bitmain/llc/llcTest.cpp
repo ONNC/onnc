ffea1d4e292170aa3cbed5380e8e0e22587e7732
zakk.chen <zakk.chen@bitmain.com>
2018-03-08 19:16:57 +0000

    add updateOutputDims unittest

--- tools/unittests/bitmain/llc/llcTest.cpp
+++ tools/unittests/bitmain/llc/llcTest.cpp
@@ -4,6 +4,7 @@
 #include "TGOperator.h"
 #include <memory>
 #include <iostream>
+#include "TGBackend.h"
 
 
 namespace {
@@ -95,6 +96,113 @@ SKYPAT_F(llcTest, testTotalWeightSize){
   std::unique_ptr<TGOperator> tgOp2(
       TGOperator::makeTGOperator(*convNode2, tgOp->getTotalWeightSize()));
   ASSERT_EQ(139776, tgOp2->getTotalWeightSize());
+}
+
+SKYPAT_F(llcTest, testOutDims) {
+  onnx::Graph graph;
+
+  // add intpu
+  onnx::Value *input1 = graph.addInput();
+  initValue(input1, "input", 1, 3, 227, 227, onnx::TensorProto_DataType_FLOAT);
+
+  // add value
+  onnx::Value *weight1 = graph.addInput();
+  initValue(weight1, "weight1", 96, 3, 11, 11,
+            onnx::TensorProto_DataType_FLOAT);
+
+  onnx::Value *bias1 = graph.addInput();
+  initValue(bias1, "bias1", 96, 1, 1, 1, onnx::TensorProto_DataType_FLOAT);
+
+  // add addInitializer
+  onnx::Tensor tWeight1;
+  graph.addInitializer(tWeight1, "weight1");
+  onnx::Tensor tBias1;
+  graph.addInitializer(tBias1, "bias1");
+
+  // create Conv node
+  onnx::Node *convNode1 = graph.create(onnx::Symbol("Conv"));
+  convNode1->addInput(input1);
+  convNode1->addInput(weight1);
+  convNode1->addInput(bias1);
+
+  {
+    std::vector<int64_t> ints;
+    for (int i = 0; i < 2; i++) {
+      ints.push_back(11);
+    }
+    convNode1->is_(onnx::Symbol("kernel_shape"), std::move(ints));
+  }
+
+  {
+    std::vector<int64_t> ints;
+    for (int i = 0; i < 2; i++) {
+      ints.push_back(4);
+    }
+    convNode1->is_(onnx::Symbol("strides"), std::move(ints));
+  }
+
+  // init output
+  onnx::Value *convOutVal1 = convNode1->outputs()[0];
+  graph.appendNode(convNode1);
+
+  ////////////////////////////////////////////////////////////////////////
+  // create MaxPool node
+  onnx::Node *maxPoolNode2 = graph.create(onnx::Symbol("MaxPool"));
+  maxPoolNode2->addInput(convOutVal1);
+  {
+    std::vector<int64_t> ints;
+    for (int i = 0; i < 2; i++) {
+      ints.push_back(3);
+    }
+    maxPoolNode2->is_(onnx::Symbol("kernel_shape"), std::move(ints));
+  }
+
+  {
+    std::vector<int64_t> ints;
+    for (int i = 0; i < 2; i++) {
+      ints.push_back(2);
+    }
+    maxPoolNode2->is_(onnx::Symbol("strides"), std::move(ints));
+  }
+
+  // init output
+  onnx::Value *maxPoolOutVal2 = maxPoolNode2->outputs()[0];
+  graph.appendNode(maxPoolNode2);
+
+  // register graph output
+  graph.registerOutput(maxPoolOutVal2);
+
+  // before update
+  auto outConvValue = convNode1->outputs()[0];
+  for (auto &i : outConvValue->sizes()) {
+    std::cout << i.dim << ",";
+  }
+  std::cout << std::endl;
+  auto outPoolValue = maxPoolNode2->outputs()[0];
+  for (auto &i : outPoolValue->sizes()) {
+    std::cout << i.dim << ",";
+  }
+  std::cout << std::endl;
+
+  ASSERT_EQ(outConvValue->sizes().size(), 0);
+  ASSERT_EQ(outPoolValue->sizes().size(), 0);
+
+  ::updateOutputDimPass::updateOutputDim(graph);
+  // after update
+  std::vector<int64_t> outConvAns{ 1, 96, 55, 55 };
+  std::vector<int64_t> outPoolAns{ 1, 96, 27, 27 };
+  int idx = 0;
+  for (auto &i : outConvValue->sizes()) {
+    std::cout << i.dim << ",";
+    ASSERT_EQ(outConvAns[idx++], i.dim);
+  }
+  std::cout << std::endl;
+  idx = 0;
+  for (auto &i : outPoolValue->sizes()) {
+    std::cout << i.dim << ",";
+    ASSERT_EQ(outPoolAns[idx++], i.dim);
+  }
+  std::cout << std::endl;
 }
 
 int main(int argc, char* argv[])
