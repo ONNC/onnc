16872ccc0c7927eae27b94161a20d2f102e695a1
zakk.chen <zakk.chen@bitmain.com>
2018-03-14 06:44:48 +0000

    add options for onnx2tg

--- tools/onnx2tg/main.cpp
+++ tools/onnx2tg/main.cpp
@@ -11,6 +11,31 @@
 
 using namespace onnc;
 
+static const char* HelpManual =
+  "Usage:\n"
+  "\tonnx2tg [input] -o [output]\n"
+  "\n"
+  "General Options:\n"
+  "\t-o <path>        Output file path\n"
+  "\n"
+  "\t-h | -? | --help Show this manual\n"
+  "onnx2tg version 0.1.0\n";
+
+static cl::opt<Path> OptInput("input", cl::kPositional, cl::kRequired,
+                              cl::kValueRequired,
+                              cl::desc("The input file"), cl::help(HelpManual));
+
+static cl::opt<Path> OptOutput("o", cl::kShort, cl::kOptional,
+                               cl::kValueRequired, cl::desc("The output file"),
+                               cl::help(HelpManual));
+
+static cl::opt<bool> OptHelp("help", cl::kLong, cl::kOptional,
+                             cl::kValueDisallowed, cl::init(false),
+                             cl::desc("Show this manual."));
+
+static cl::alias HelpAliasH("h", cl::kShort, cl::trueopt(OptHelp));
+static cl::alias HelpAliasQ("?", cl::kShort, cl::trueopt(OptHelp));
+
 //===----------------------------------------------------------------------===//
 // Main procedure
 //===----------------------------------------------------------------------===//
