edc83af1f8b6c354d513883530a0d5052eab73df
Luba tang <luba@skymizer.com>
2018-05-08 11:38:11 +0800

    Separate NPUTargetBackend and DLATargetBackend from general TargetBackend.
    These two backends shall have their own unique TargetInfo.

--- include/onnc/Core/PassSupport.h
+++ include/onnc/Core/PassSupport.h
@@ -23,22 +23,33 @@ class TargetBackend;
     return info;                                                              \
   }
 
-#define INITIALIZE_TB_PASS(pass_name, name)                                   \
+#define INITIALIZE_TB_PASS_BASE(pass_name, backend_type, name)                \
   static void *Initialize##pass_name##Pass(PassRegistry &pRegistry) {         \
     PassInfo *info = new PassInfo(                                            \
       name,                                                                   \
       &pass_name::ID,                                                         \
       PassInfo::PassCtorFn(callDefaultCtor<pass_name>),                       \
-      PassInfo::PassTargetCtorFn(callTargetBackendCtor<pass_name>));          \
+      PassInfo::PassTargetCtorFn(                                             \
+          callTargetBackendCtor<pass_name, ##backend_type##>));               \
     pRegistry.registerPass(*info);                                            \
     return info;                                                              \
   }
+
+#define INITIALIZE_TB_PASS(pass_name, name) \
+   INITIALIZE_TB_PASS_BASE(pass_name, TargetBackend, name)
+
+#define INITIALIZE_DLA_PASS(pass_name, name) \
+   INITIALIZE_TB_PASS_BASE(pass_name, DLATargetBackend, name)
+
+#define INITIALIZE_NPU_PASS(pass_name, name) \
+   INITIALIZE_TB_PASS_BASE(pass_name, NPUTargetBackend, name)
 
 /// Default constructor wrapper. A wrapper to convert a constructor into normal
 /// function.
 template <typename PassName> Pass *callDefaultCtor() { return new PassName(); }
 
-template <typename PassName> Pass *callTargetBackendCtor(TargetBackend* pTB) {
+template <typename PassName, typename BackendType>
+Pass *callTargetBackendCtor(BackendType* pTB) {
   return new PassName(pTB);
 }
 
