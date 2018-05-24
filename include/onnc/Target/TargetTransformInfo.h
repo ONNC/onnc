8dfb4ce16bfa720962547ae3e73abcb040b86965
Chuang-Yu Cheng <cycheng@skymizer.com>
2018-05-24 11:47:53 +0900

    [TargetTransformInfo] Add getOperatorMemUsage for getting total memory usage of onnx node.

--- include/onnc/Target/TargetTransformInfo.h
+++ include/onnc/Target/TargetTransformInfo.h
@@ -8,8 +8,12 @@
 #ifndef ONNC_TARGET_TRANSFORM_INFO_H
 #define ONNC_TARGET_TRANSFORM_INFO_H
 #include <onnx/common/ir.h>
+#include <onnc/Target/TargetMemInfo.h>
+#include <vector>
 
 namespace onnc {
+
+using LongInts = std::vector<int64_t>;
 
 /** \class TargetTransformInfo
  *  \brief TargetTransformInfo provides interfaces to explore target
@@ -26,6 +30,25 @@ public:
                               enum TargetCostKind kind) const
   {
     return 0;
+  }
+
+  /// Get coarse-grained (approximately) total memory usage of onnx node.
+  ///
+  /// Memory usage of onnx node is target dependent. E.g. a gemm node with
+  /// A [N x K], B [K x M], C [N x M], its total memory usage is
+  /// (N * K + K * N + N * M) * (Element Size), however, target's might
+  /// choose to load part of rows and columns to compute a tile, so its
+  /// real memory usage will be reduced a lot.
+  virtual MemSize getOperatorMemUsage(const onnx::Node *pNode) const
+  {
+    return MemSize();
+  }
+
+  virtual MemSize getOperatorMemUsage(const onnx::Node *pNode,
+                                const std::vector<LongInts> &pInputSizes,
+                                const std::vector<LongInts> &pOutputSizes) const
+  {
+    return MemSize();
   }
 
   virtual int getWarpSize() const { return 0; }
