78760c6d9e69b229e36111086056028a8133ac9f
Chuang-Yu Cheng <cycheng@skymizer.com>
2018-05-30 17:07:14 +0900

    [NodeIRScheduler] Implement greedy approach to reorder IR in topological order.
    Based on target capability, reorder onnx IR for better exe performance.

--- include/onnc/Analysis/NodeIRScheduler.h
+++ include/onnc/Analysis/NodeIRScheduler.h
@@ -15,12 +15,35 @@
 #include <onnc/Core/ModulePass.h>
 #include <onnc/Core/PassSupport.h>
 #include <onnc/Target/DLATargetBackend.h>
-#include <onnx/common/ir.h>
+#include <onnc/Target/TargetTransformInfo.h>
+#include <tuple>
+#include <unordered_map>
+#include <vector>
 
 namespace onnc {
 
+using Nodes = std::vector<onnx::Node *>;
+using Ints = std::vector<int>;
+
+/** \class NodeIRScheduler
+ *  \brief onnx Graph IR scheduler. Reorder IR, especially load/store for
+ *         better, even maximum performance.
+ */
 class NodeIRScheduler : public ModulePass
 {
+public:
+  struct ExeResUser
+  {
+    onnx::Node *user;
+    unsigned remainCycles;  /// Remaining cycles to finish executing the user.
+
+    ExeResUser(onnx::Node *user, unsigned rcycles)
+      : user(user), remainCycles(rcycles) {}
+  };
+
+  // <execution resource, list of <user, remaining cycles>>
+  using ExeResUserMap = std::unordered_map<const ExeResource*,
+                                           std::vector<ExeResUser>>;
 public:
   static char ID;
 
@@ -36,7 +59,20 @@ public:
   void print(std::ostream& pOS) const;
 
 private:
+  Nodes greedyPickNextNodes(Nodes &pCands);
+
+  bool isExeResAvailable(const ExeResource *pExeRes) const;
+
+  void addExeResUser(const ExeResource *pExeRes, onnx::Node *pUser);
+
+  unsigned updateResList();
+
+  void clear() { m_NewIROrder.clear(); }
+
+private:
   DLATargetBackend* m_DLATB;
+  Nodes m_NewIROrder;
+  ExeResUserMap m_ExeResUsers;
 };
 
 NodeIRScheduler *CreateNodeIRSchedulerPass(DLATargetBackend *pDLATB);
--- lib/Analysis/NodeIRScheduler.cpp
+++ lib/Analysis/NodeIRScheduler.cpp
@@ -13,21 +13,16 @@
 #include <onnc/Support/IOStream.h>
 #include <onnc/Target/TargetTransformInfo.h>
 #include <iomanip> // for setw
+#include <limits>
 
 using namespace onnc;
-
-//===----------------------------------------------------------------------===//
-// Extension IR for onnx
-//===----------------------------------------------------------------------===//
-static const onnx::Symbol g_LoadKind("Load");
-static const onnx::Symbol g_StoreKind("Store");
 
 //===----------------------------------------------------------------------===//
 // Non-member functions
 //===----------------------------------------------------------------------===//
 static bool HasInsertedLoadStoreNode(onnx::Graph &pGraph)
 {
-  return pGraph.nodes().rbegin()->kind() == g_StoreKind;
+  return pGraph.nodes().rbegin()->kind() == onnx::Symbol("Store");
 }
 
 static void InsertLoadStoreNode(onnx::Graph &pGraph)
@@ -45,7 +40,7 @@ static void InsertLoadStoreNode(onnx::Graph &pGraph)
     }
 
     // Create load node and insert before the first use node.
-    onnx::Node* loadN = pGraph.create(g_LoadKind);
+    onnx::Node* loadN = pGraph.create(onnx::Symbol("Load"));
     loadN->insertBefore(first);
     loadN->output()->copyMetadata(v);
     v->replaceAllUsesWith(loadN->output());
@@ -64,7 +59,7 @@ static void InsertLoadStoreNode(onnx::Graph &pGraph)
     }
 
     // Create store node and insert before the last use node.
-    onnx::Node* storeN = pGraph.create(g_StoreKind, {v}/*, 0*/);
+    onnx::Node* storeN = pGraph.create(onnx::Symbol("Store"), {v}/*, 0*/);
     storeN->output()->copyMetadata(v);
     storeN->insertBefore(last);
   }
@@ -81,6 +76,91 @@ NodeIRScheduler::~NodeIRScheduler()
 {
 }
 
+using DegreeMap = std::unordered_map<onnx::Node *, unsigned>;
+
+static DegreeMap BuildDegreeMap(onnx::Graph &pGraph)
+{
+  DegreeMap dmap;
+  for (onnx::Node *n : pGraph.nodes()) {
+    if (n->kind() == onnx::kUndefined)
+      continue;
+
+    unsigned degcnt = n->inputs().size();
+    for (onnx::Value *v : n->inputs())
+      if (v->node() == nullptr) {
+        outs() << "Warning! " << n->kind().toString()
+               << " use a value = " << v->uniqueName()
+               << ", which doen't bind to a node";
+        --degcnt;
+      }
+    dmap[n] = degcnt;
+  }
+  return dmap;
+}
+
+bool NodeIRScheduler::isExeResAvailable(const ExeResource *pExeRes) const
+{
+  return m_ExeResUsers.find(pExeRes)->second.size() < pExeRes->count;
+}
+
+void NodeIRScheduler::addExeResUser(const ExeResource *pExeRes,
+                                    onnx::Node *pUser)
+{
+  int c = m_DLATB->getTTI()
+                 ->getOperatorCost(pUser, TargetTransformInfo::kCycleCount);
+  m_ExeResUsers[pExeRes].emplace_back(pUser, c);
+}
+
+unsigned NodeIRScheduler::updateResList()
+{
+  // find min cycle count
+  unsigned minCycle = std::numeric_limits<unsigned>::max();
+  for (auto &it : m_ExeResUsers)
+    for (auto &user : it.second)
+      minCycle = std::min(minCycle, user.remainCycles);
+
+  // update cycle count based on min cycle count.
+  for (auto &it : m_ExeResUsers) {
+    std::vector<unsigned> rmList;
+    auto &userList = it.second;
+    for (unsigned i = 0; i < userList.size(); ++i) {
+      userList[i].remainCycles -= minCycle;
+      if (userList[i].remainCycles == 0)
+        rmList.push_back(i);
+    }
+
+    // release exe resource.
+    for (int i = rmList.size() - 1; i >= 0; --i)
+      userList.erase(userList.begin() + i);
+  }
+  return minCycle;
+}
+
+Nodes NodeIRScheduler::greedyPickNextNodes(Nodes &pCands)
+{
+  Nodes next;
+  Ints rmList;
+
+  for (unsigned i = 0; i < pCands.size(); ++i) {
+    onnx::Node *n = pCands[i];
+    const ExeResource *res = m_DLATB->getTTI()->queryExeResType(n);
+    // initialize entry with empty user.
+    if (m_ExeResUsers.find(res) == m_ExeResUsers.end())
+      m_ExeResUsers[res] = {};
+
+    if (isExeResAvailable(res)) {
+      addExeResUser(res, n);
+      next.push_back(n);
+      rmList.push_back(i);
+    }
+  }
+
+  for (auto it = rmList.rbegin(); it != rmList.rend(); ++it)
+    pCands.erase(pCands.begin() + *it);
+
+  return next;
+}
+
 Pass::ReturnType NodeIRScheduler::runOnModule(Module& pModule)
 {
   if (!m_DLATB) {
@@ -88,20 +168,43 @@ Pass::ReturnType NodeIRScheduler::runOnModule(Module& pModule)
     return kPassFailure;
   }
 
+  clear();
+
   onnx::Graph &graph = *pModule.getGraph();
   if (!HasInsertedLoadStoreNode(graph));
     InsertLoadStoreNode(graph);
 
+  DegreeMap dmap = BuildDegreeMap(graph);
+  Nodes worklist;
+
+  // add degree = 0 to worklist in graph order.
   for (onnx::Node *n : graph.nodes()) {
     if (n->kind() == onnx::kUndefined)
       continue;
 
-    int c = m_DLATB->getTTI()
-                   ->getOperatorCost(n, TargetTransformInfo::kCycleCount);
-
-    outs() << std::setw(12) << std::left << c;
-    PrintNode(outs(), *n);
+    if (dmap[n] == 0)
+      worklist.push_back(n);
   }
+
+  while (!worklist.empty()) {
+    // worklist is modified in greedyPickNextNodes
+    for (onnx::Node *n : greedyPickNextNodes(worklist)) {
+      for (onnx::Value *v : n->outputs()) {
+        // Update degree map.
+        for(auto u : v->uses()) {
+          if (u.user->kind() == onnx::kReturn)
+            continue;
+          auto it = dmap.find(u.user);
+          assert(it != dmap.end() &&
+                 "Node doesn't exist in dmap!?");
+          // --Degree
+          it->second -= 1;
+          if (it->second == 0)
+            worklist.push_back(it->first);
+        } // for each user of this value.
+      } // for each output value.
+    } // for each picked node.
+  } // while !worklist.empty()
 
   return kModuleNoChanged;
 }
