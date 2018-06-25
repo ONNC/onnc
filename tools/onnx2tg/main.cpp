b8f58f79331577bc6dfc6c37c5c39ec93b6eb989
Eric Yu <eric.yu@bitmain.com>
2018-06-25 12:02:16 +0800

    add AboutData for onnx2tg

--- tools/onnx2tg/main.cpp
+++ tools/onnx2tg/main.cpp
@@ -16,61 +16,49 @@
 
 using namespace onnc;
 
-static const char *HelpManual = "Usage:\n"
-                                "\tonnx2tg [input] -o [output]\n"
-                                "\n"
-                                "General Options:\n"
-                                "\t-o <path>        Output file path\n"
-                                "\n"
-                                "\t-march [bm1680|bm1880]        \n"
-                                "\n"
-                                "\t-print-module-before-isel      \n"
-                                "\n"
-                                "\t-print-machineinstrs           \n"
-                                "\n"
-                                "\t-ignore-calibration-step       \n"
-                                "\n"
-                                "\t-h | -? | --help Show this manual\n"
-                                "onnx2tg version 0.1.0\n";
+static AboutData g_About("onnx2tg", "onnc2tg", "0.1.0", AboutLicense::kPrivate,
+                         "ONNC is the compiler driver");
 
 static cl::opt<std::string> OptInput("input", cl::kPositional, cl::kOptional,
                                      cl::kValueRequired,
-                                     cl::desc("The input file"),
-                                     cl::help(HelpManual), cl::init("-"));
+                                     cl::desc("The input file"), cl::init("-"),
+                                     cl::about(g_About));
 
 static cl::opt<std::string> OptOutput("o", cl::kShort, cl::kOptional,
                                       cl::kValueRequired,
                                       cl::desc("The output file"),
-                                      cl::help(HelpManual),
-                                      cl::init("cmdbuf.bin"));
+                                      cl::init("cmdbuf.bin"),
+                                      cl::about(g_About));
 
 static cl::opt<std::string> march("march", cl::kShort, cl::kOptional,
                                   cl::kValueRequired,
                                   cl::desc("The march of TG [bm1680|bm1880]"),
-                                  cl::help(HelpManual), cl::init("bm1880"));
+                                  cl::init("bm1880"), cl::about(g_About));
 
 static cl::opt<bool>
     PrintMachineCode("print-machineinstrs", cl::kShort, cl::kOptional,
                      cl::kValueDisallowed, cl::init(false),
-                     cl::desc("Print generated machine code for debugging)"));
+                     cl::desc("Print generated machine code for debugging"),
+                     cl::about(g_About));
 
 static cl::opt<bool> printModuleBeforeISel("print-module-before-isel",
                                            cl::kShort, cl::kOptional,
                                            cl::kValueDisallowed,
-                                           cl::init(false));
+                                           cl::init(false), cl::about(g_About));
 
-static cl::opt<bool> IgnoreCalibrationStep("ignore-calibration-step",
-                                           cl::kShort, cl::kOptional,
-                                           cl::kValueDisallowed,
-                                           cl::init(false),
-                                           cl::desc("ignore ctable)"));
+static cl::opt<bool>
+    IgnoreCalibrationStep("ignore-calibration-step", cl::kShort, cl::kOptional,
+                          cl::kValueDisallowed, cl::init(false),
+                          cl::desc("ignore ctable"), cl::about(g_About));
 
 static cl::opt<bool> OptHelp("help", cl::kLong, cl::kOptional,
                              cl::kValueDisallowed, cl::init(false),
-                             cl::desc("Show this manual."));
+                             cl::desc("Show this manual."), cl::about(g_About));
 
-static cl::alias HelpAliasH("h", cl::kShort, cl::trueopt(OptHelp));
-static cl::alias HelpAliasQ("?", cl::kShort, cl::trueopt(OptHelp));
+static cl::alias HelpAliasH("h", cl::kShort, cl::trueopt(OptHelp),
+                            cl::about(g_About));
+static cl::alias HelpAliasQ("?", cl::kShort, cl::trueopt(OptHelp),
+                            cl::about(g_About));
 
 //===----------------------------------------------------------------------===//
 // Main procedure
@@ -81,7 +69,7 @@ int main(int pArgc, char *pArgv[])
   InitializeAnalysisPassOptions();
   ONNX2TG onnx2tg(pArgc, pArgv);
   if (OptHelp) {
-    std::cout << HelpManual;
+    g_About.print(std::cout, true);
     exit(0);
   }
 
