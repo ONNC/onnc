fd0f916e62af2c73aefbd515ee99e928778f17c5
Luba tang <luba@skymizer.com>
2018-07-16 11:15:27 +0900

    Create InputOperators for input values.

--- lib/Transforms/PreTensorSel.cpp
+++ lib/Transforms/PreTensorSel.cpp
@@ -8,6 +8,7 @@
 #include <onnc/Transforms/PreTensorSel.h>
 #include <onnc/Core/PassSupport.h>
 #include <onnc/IR/Compute/Initializer.h>
+#include <onnc/IR/Compute/InputOperator.h>
 #include <onnc/IR/IRBuilder.h>
 #include <onnx/common/ir.h>
 
@@ -48,18 +49,20 @@ Pass::ReturnType PreTensorSel::runOnModule(::onnc::Module &pModule)
 
   for (::onnx::Value* v : graph->inputs()) {
     auto it = initializers.find(v->uniqueName());
-    if (initializers.end() == it) {
-      // If a value doesn't appear in initilizers, then we only need to keep
-      // its shape.
-      builder.CreateComputeTensor(*v);
-    }
-    else {
+    if (initializers.end() != it) {
       // The value appears in an initializer, we should create corresponding
       // initializer to handle with the value.
       Initializer* init =
           builder.AddComputeOp<onnc::Initializer>(v->uniqueName());
       Tensor* value = builder.CreateComputeTensor(*v, *it->second);
       init->setTensor(*value);
+    }
+    else { // it is an normal input value
+      // create corresponding input operator to handle with the value
+      InputOperator* input =
+          builder.AddComputeOp<onnc::InputOperator>(v->uniqueName());
+      Tensor* value = builder.CreateComputeTensor(*v);
+      input->setTensor(*value);
     }
   } // end of trip on all input values
 
