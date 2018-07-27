96a73bee0f9ccac10a26c15d06e69262017d4202
Luba tang <luba@skymizer.com>
2018-07-27 12:25:37 +0800

    Add target-dependent Store operator.

---
+++ lib/Target/TG/BM188x/Compute/Store.cpp
@@ -0,0 +1,51 @@
+//===- Store.cpp ----------------------------------------------------------===//
+//
+//                             The ONNC Project
+//
+// See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+#include "Store.h"
+#include "../BM188xVisitor.h"
+#include <onnc/Support/Casting.h>
+#define DEBUG_TYPE "tl_store"
+
+using namespace onnc;
+using namespace onnc::BM188X;
+
+//===----------------------------------------------------------------------===//
+// Store
+//===----------------------------------------------------------------------===//
+BM188X::Store::Store(const IntAttr& pDstGOffset,
+             const IntAttr& pSrcLAddr,
+             const BoolAttr& pDoTranspose,
+             const BoolAttr& pIsAligned,
+             const BoolAttr& pIsNeuron,
+             const IntsAttr& pLocalDim,
+             const IntsAttr& pGlobalDim,
+             const StringAttr& pSplitName)
+    : ComputeOperator("Store"), m_DstGOffset(pDstGOffset),
+      m_SrcLAddr(pSrcLAddr), m_DoTranspose(pDoTranspose),
+      m_IsAligned(pIsAligned), m_IsNeuron(pIsNeuron), m_LocalDim(pLocalDim),
+      m_GlobalDim(pGlobalDim), m_SplitName(pSplitName)
+{
+}
+
+void BM188X::Store::print(std::ostream& pOS) const
+{
+  // TODO
+}
+
+void BM188X::Store::accept(ComputeVisitor& pV)
+{
+  BM188xVisitor* visitor = dyn_cast<BM188xVisitor>(&pV);
+  if (nullptr != visitor)
+    visitor->visit(*this);
+}
+
+void BM188X::Store::accept(ComputeVisitor& pV) const
+{
+  BM188xVisitor* visitor = dyn_cast<BM188xVisitor>(&pV);
+  if (nullptr != visitor)
+    visitor->visit(*this);
+}
---
+++ lib/Target/TG/BM188x/Compute/Store.h
@@ -0,0 +1,81 @@
+//===- Store.h ------------------------------------------------------------===//
+//
+//                             The ONNC Project
+//
+// See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+#ifndef ONNC_TARGET_BM1880_TL_STORE_H
+#define ONNC_TARGET_BM1880_TL_STORE_H
+#include <onnc/IR/ComputeOperator.h>
+#include <onnc/IR/Compute/Attributes.h>
+
+namespace onnc {
+namespace BM188X {
+
+class Store : public ComputeOperator
+{
+public:
+  Store(const IntAttr& pDstGOffset,
+        const IntAttr& pSrcLAddr,
+        const BoolAttr& pDoTranspose,
+        const BoolAttr& pIsAligned,
+        const BoolAttr& pIsNeuron,
+        const IntsAttr& pLocalDim,
+        const IntsAttr& pGlobalDim,
+        const StringAttr& pSplitName);
+
+  const IntAttr& getDstGOffset() const { return m_DstGOffset; }
+
+  const IntAttr& getSrcLAddr() const { return m_SrcLAddr; }
+
+  const BoolAttr& getDoTranspose() const { return m_DoTranspose; }
+
+  const BoolAttr& getIsAligned() const { return m_IsAligned; }
+
+  const BoolAttr& getIsNeuron() const { return m_IsNeuron; }
+
+  const IntsAttr& getLocalDim() const { return m_LocalDim; }
+
+  const IntsAttr& getGlobalDim() const { return m_GlobalDim; }
+
+  const StringAttr& getSplitName() const { return m_SplitName; }
+
+  void setDstGOffset(const IntAttr& pOffset) { m_DstGOffset = pOffset; }
+
+  void setSrcLAddr(const IntAttr& pSrcLAddr) { m_SrcLAddr = pSrcLAddr; }
+
+  void setDoTranspose(const BoolAttr& pT) { m_DoTranspose = pT; }
+
+  void setIsAligned(bool pIsAligned) { m_IsAligned = pIsAligned; }
+
+  void setIsNeuron(bool pIsNeuron) { m_IsNeuron = pIsNeuron; }
+
+  void setSplitName(const std::string& pSplitName) { m_SplitName = pSplitName; }
+
+  void setLocalDim(const IntsAttr& pLocalDim) { m_LocalDim = pLocalDim; }
+
+  void setGlobalDim(const IntsAttr& pGlobalDim) { m_GlobalDim = pGlobalDim; }
+
+  void print(std::ostream& pOS) const override;
+
+  void accept(ComputeVisitor& pV) override;
+
+  void accept(ComputeVisitor& pV) const override;
+
+private:
+  IntAttr m_DstGOffset;
+  IntAttr m_SrcLAddr;
+  BoolAttr m_DoTranspose;
+  BoolAttr m_IsAligned;
+  BoolAttr m_IsNeuron;
+  IntsAttr m_LocalDim;
+  IntsAttr m_GlobalDim;
+  StringAttr m_SplitName;
+
+};
+
+} // namespace BM188X
+} // namespace onnc
+
+#endif
