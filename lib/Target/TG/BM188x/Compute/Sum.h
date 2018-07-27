2f897ff11ee90fad9c9febf71655225fa62bcb52
Luba tang <luba@skymizer.com>
2018-07-27 14:21:15 +0800

    Implement BM188X::Sum.

---
+++ lib/Target/TG/BM188x/Compute/Sum.h
@@ -0,0 +1,47 @@
+//===- Sum.h --------------------------------------------------------------===//
+//
+//                             The ONNC Project
+//
+// See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+#ifndef ONNC_TARGET_BM1880_SUM_H
+#define ONNC_TARGET_BM1880_SUM_H
+#include <onnc/IR/ComputeOperator.h>
+#include <onnc/IR/Compute/Attributes.h>
+#include <onnc/IR/Compute/Sum.h>
+
+namespace onnc {
+namespace BM188X {
+
+class Sum : public onnc::Sum
+{
+public:
+  Sum();
+
+  const IntAttr &getRShiftWidth() const { return m_RShiftWidth; }
+
+  const IntAttr &getDoRelu() const { return m_DoRelu; }
+
+  const IntsAttr &getThresholdXQuantized() const {
+    return m_ThresholdXQuantized;
+  }
+
+  void setRShiftWidth(const IntAttr &pRSW) { m_RShiftWidth = pRSW; }
+
+  void setDoRelu(const IntAttr &pDoRelu) { m_DoRelu = pDoRelu; }
+
+  void setThresholdXQuantized(const IntsAttr &pT ) {
+    m_ThresholdXQuantized = pT;
+  }
+
+private:
+  IntAttr m_RShiftWidth;
+  IntAttr m_DoRelu;
+  IntsAttr m_ThresholdXQuantized;
+};
+
+} // namespace BM188X
+} // namespace onnc
+
+#endif
