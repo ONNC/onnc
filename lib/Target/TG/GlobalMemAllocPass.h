223d9cfaeffd29e2bbedeaa489b522e0b16951cf
Chuang-Yu Cheng <cycheng@skymizer.com>
2018-07-28 23:40:25 +0800

    Implement TG memory allocation algorithm

--- lib/Target/TG/GlobalMemAllocPass.h
+++ lib/Target/TG/GlobalMemAllocPass.h
@@ -11,12 +11,22 @@
 #include "TGBackend.h"
 #include <onnc/Core/ModulePass.h>
 #include <onnc/Core/PassSupport.h>
+#include <vector>
+#include <unordered_set>
+#include <utility>
+
+class GlobalMemAllocVisitor;
 
 namespace onnc {
 namespace BM188X {
 
 class GlobalMemAlloc : public ModulePass
 {
+  friend class ::GlobalMemAllocVisitor;
+public:
+  typedef std::vector<std::pair<ComputeMemOperand*, onnc::Value*>>
+          MemOperandValList;
+
 public:
   static char ID;
 
@@ -28,12 +38,20 @@ public:
   Pass::ReturnType runOnModule(::onnc::Module &pModule) override;
 
 private:
+  void createMemOperandsOfNode(ComputeGraph &pCG, ComputeOperator &pNode,
+                               ComputeOperand::Residence pResd);
+
   void createAllMemOperands(onnc::ComputeGraph &pCG);
 
   void allocGlobalMem();
 
+  void linearScanAlloMem();
+
+  void clear();
+
 private:
   TGBackend *m_pTarget; // NOLINT
+  MemOperandValList m_ValOperandMap;
 };
 
 } // namespace BM188X
