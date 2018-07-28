fc67fd345597811abe2d2e0486fabecd022bfad0
Chuang-Yu Cheng <cycheng@skymizer.com>
2018-07-28 16:14:23 +0800

    Implement TG memory allocation visitor.

--- lib/Target/TG/GlobalMemAllocPass.cpp
+++ lib/Target/TG/GlobalMemAllocPass.cpp
@@ -6,11 +6,13 @@
 //
 //===----------------------------------------------------------------------===//
 #include "GlobalMemAllocPass.h"
+#include "BM188x/BM188xVisitor.h"
 #include "TG.h"
 #include "TGBackend.h"
 #include <onnc/ADT/Color.h>
 #include <onnc/Core/ModulePass.h>
 #include <onnc/Core/PassSupport.h>
+#include <onnc/IR/Compute/Initializer.h>
 #include <onnc/Support/Debug.h>
 #include <onnc/Support/IOStream.h>
 #include <onnx/common/ir.h>
@@ -22,6 +24,45 @@ using namespace onnc::BM188X;
 
 char GlobalMemAlloc::ID = 0;
 
+static void CreateMemOperands(onnc::ComputeGraph &pCG, ComputeOperator &pNode,
+                              ComputeOperand::Residence pResd)
+{
+  unsigned int out_size = pNode.getNumOfOutputs();
+  for (unsigned int i = 0; i < out_size; ++i) {
+    onnc::Value* value = pNode.getOutput(i);
+    onnc::Value::UseList& use_list = value->getUses();
+    onnc::Value::UseList::iterator use, uEnd = use_list.end();
+    for (use = use_list.begin(); use != uEnd; ++use)
+      pCG.addOperand<ComputeMemOperand>(pNode, *use->getUser(),
+                                        *value, pResd);
+  }
+}
+
+// For handle special cases.
+class GlobalMemAllocVisitor : public BM188X::BM188xVisitor
+{
+public:
+  GlobalMemAllocVisitor(onnc::ComputeGraph &pCG)
+    : m_Processed(false), m_CG(pCG) {
+  }
+
+  void visit(Initializer &pInitializer) override
+  {
+    m_Processed = true;
+    CreateMemOperands(m_CG, pInitializer, ComputeOperand::kWeightResidence);
+  }
+
+  bool isProcessed() const { return m_Processed; }
+
+  void reset() { m_Processed = false; }
+
+  // TLLoad TLStore
+
+private:
+  bool m_Processed;
+  onnc::ComputeGraph &m_CG;
+};
+
 //===----------------------------------------------------------------------===//
 // GlobalMemAlloc
 //===----------------------------------------------------------------------===//
@@ -30,7 +71,34 @@ GlobalMemAlloc::GlobalMemAlloc(TGBackend *pTarget)
 {
 }
 
-Pass::ReturnType GlobalMemAlloc::runOnModule(::onnc::Module &pModule)
+Pass::ReturnType GlobalMemAlloc::runOnModule(::onnc::Module &pModule) override
+{
+  AllocGlobalMem(); // remove this later.
+
+  Module::cg_iterator cg, cgEnd = pModule.cgEnd();
+  for (cg = pModule.cgBegin(); cg != cgEnd; ++cg)
+    runOnComputeGraph(*cg->value());
+
+  return Pass::kModuleNoChanged;
+}
+
+void GlobalMemAlloc::runOnComputeGraph(onnc::ComputeGraph &pCG)
+{
+  GlobalMemAllocVisitor gmallocVisitor(pCG);
+  ComputeGraph::iterator nodeIt, nEnd = pCG.end();
+  for (nodeIt = pCG.begin(); nodeIt != nEnd; ++nodeIt) {
+    // process special cases.
+    gmallocVisitor.reset();
+    nodeIt->accept(gmallocVisitor);
+    if (gmallocVisitor.isProcessed())
+      continue;
+
+    // process general case.
+    CreateMemOperands(pCG, *nodeIt, ComputeOperand::kOutputResidence);
+  }
+}
+
+void GlobalMemAlloc::AllocGlobalMem()
 {
   unsigned int weight_offset = 0;
   unsigned int neuron_offset = 0;
@@ -69,7 +137,7 @@ Pass::ReturnType GlobalMemAlloc::runOnModule(::onnc::Module &pModule)
   return kModuleNoChanged;
 }
 
-ModulePass *onnc::CreateGlobalMemAllocPass(TGBackend *pTarget)
+ModulePass *onnc::createGlobalMemAllocPass(TGBackend *pTarget)
 {
   return new GlobalMemAlloc(pTarget);
 }
