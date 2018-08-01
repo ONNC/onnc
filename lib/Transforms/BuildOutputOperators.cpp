82a6236b667b93bad950936307c92c6c1d26f43b
Chuang-Yu Cheng <cycheng@skymizer.com>
2018-08-01 22:01:21 +0800

    OutputOperator: Fix bug, add value to input list.

--- lib/Transforms/BuildOutputOperators.cpp
+++ lib/Transforms/BuildOutputOperators.cpp
@@ -21,8 +21,14 @@ char BuildOutputOperators::ID = 0;
 Pass::ReturnType
 BuildOutputOperators::runOnGraphs(::onnx::Graph& pTG, ComputeGraph& pCG)
 {
-  for (::onnx::Value* v : pTG.outputs())
-    pCG.addOperator<onnc::OutputOperator>(v->uniqueName());
+  for (::onnx::Value* v : pTG.outputs()) {
+    OutputOperator* op = pCG.addOperator<onnc::OutputOperator>(v->uniqueName());
+    // add v as an input.
+    onnc::Tensor* tensor = pCG.getValue<onnc::Tensor>(v->uniqueName());
+    if (nullptr == tensor)
+      tensor = IRBuilder::CreateComputeTensor(pCG, *v);
+    op->addInput(*tensor);
+  }
 
   return Pass::kModuleChanged;
 }
