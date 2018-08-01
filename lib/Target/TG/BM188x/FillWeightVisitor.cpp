fea617f9ea295f10785f3b181f0f335b315fb9e2
Luba Tang <luba@skymizer.com>
2018-08-01 17:31:40 +0800

    Add support member functions.

--- lib/Target/TG/BM188x/FillWeightVisitor.cpp
+++ lib/Target/TG/BM188x/FillWeightVisitor.cpp
@@ -17,72 +17,72 @@ using namespace onnc::BM188X;
 // FillWeightVisitor
 //===----------------------------------------------------------------------===//
 FillWeightVisitor::FillWeightVisitor(GenWeightPass::WeightType& pWeight)
-    : m_Weight(pWeight)
+    : m_Weight(pWeight), m_DoneOpndSet()
 {
 }
 
 void FillWeightVisitor::visit(const onnc::Initializer &pOp)
 {
-  if (onnc::Value::kInt8 == pOp.getTensor<onnc::Value>()->kind()) {
-  }
-  else {
-  }
+  // TODO
 }
 
+// visit TGConv
 void FillWeightVisitor::visit(const BM188X::Conv& pConv)
 {
-/**
-  const onnx::Value *value = pTGConv.getMemOperand(1)->m_Value;
-  const onnx::Tensor &tensor =
-        onnc::getTensor(value->uniqueName(), *value->owningGraph());
-  assert(pTGConv.getMemOperand(1)->m_Type == onnx::TensorProto_DataType_INT8);
-  assert(tensor.is_raw_data());
-  const std::string &raw = tensor.raw();
-  size_t count = onnc::getTotalCount(tensor.sizes());
-**/
+  // get weight value V from pConv.input(1)
+  // create a weight matrix W whose size is V's dimension 
+  // copy V's value to a data D
   GenWeightPass::WeightType weight;
-/**
-  weight.resize(count);
-  std::vector<int8_t> data;
-  std::copy(raw.begin(), raw.end(), std::back_inserter(data));
-
-  // conv weight is arranged by (1, oc, kh*kw, ic)
-  // convert (oc, ic, kh, kw) to (1, oc, kh*kw, ic)
-  int ic = pTGConv.getInC() / pTGConv.getGroups();
-  for (int oc_i = 0; oc_i < pTGConv.getOutC(); ++oc_i) {
-    for (int k_i = 0; k_i < pTGConv.getKH() * pTGConv.getKW(); ++k_i) {
-      for (int ic_i = 0; ic_i < ic; ++ic_i) {
-        int to = oc_i * (pTGConv.getKH() * pTGConv.getKW() * ic) + k_i * ic +
-                 ic_i;
-        int from = oc_i * (pTGConv.getKH() * pTGConv.getKW() * ic) +
-                   ic_i * (pTGConv.getKH() * pTGConv.getKW()) + k_i;
-        weight[to] = data[from];
-      }
-    }
-  }
-
-  // 16bit bias
-  if (pTGConv.getDoBias() == 1) {
-    auto *mem_op = pTGConv.getMemOperand(pTGConv.getBiasIdx());
-    Weight::prepare16bitWeight(*mem_op, weight);
-  }
-
-  // 8bit scale bias
-  if (pTGConv.getDoScale() == 1) {
-    auto *mem_op = pTGConv.getMemOperand(pTGConv.getScaleIdx());
-    Weight::prepare8bitWeight(*mem_op, weight);
-  }
-
-  // 16bit scale bias
-  if (pTGConv.getDoScaleBias() == 1) {
-    auto *mem_op = pTGConv.getMemOperand(pTGConv.getScaleBiasIdx());
-    Weight::prepare16bitWeight(*mem_op, weight);
-  }
-**/
-  // update weight
   m_Weight.insert(m_Weight.end(), weight.begin(), weight.end());
 }
 
+// visit TLConv
 void FillWeightVisitor::visit(const BM188X::SlicedConv& pSlicedConv)
 {
+  // TODO
+}
+
+//===----------------------------------------------------------------------===//
+// FillWeightVisitor support members
+//===----------------------------------------------------------------------===//
+void BM188X::FillWeightVisitor::Convert(Weight& pWeight, const std::string& pRaw,
+                      int pKS, int pIC, int pOC)
+{
+  // conv weight is arranged by (1, oc, kh*kw, ic)
+  // convert (oc, ic, kh, kw) to (1, oc, kh*kw, ic)
+  for (int oc_i = 0; oc_i < pOC; ++oc_i) {
+    for (int k_i = 0; k_i < pKS; ++k_i) {
+      for (int ic_i = 0; ic_i < pIC; ++ic_i) {
+        int to   = oc_i*pKS*pIC + k_i  * pIC + ic_i;
+        int from = oc_i*pKS*pIC + ic_i * pKS + k_i;
+        pWeight[to] = (int8_t)pRaw[from];
+      }
+    }
+  }
+}
+
+void BM188X::FillWeightVisitor::Append8bit(Weight& pW, const std::string &pRaw)
+{
+  std::copy(pRaw.begin(), pRaw.end(), std::back_inserter(pW));
+}
+
+void BM188X::FillWeightVisitor::Append16bit(Weight& pW, const std::string &pRaw)
+{
+  size_t count = pRaw.size();
+  size_t offset = pW.size();
+  pW.resize(offset + count * 2);
+  for (size_t i = 0; i*2 < count; ++i) {
+    pW[offset + i] = (int8_t)pRaw[i*2];
+    pW[offset + i + count] = (int8_t)pRaw[i*2 + 1];
+  }
+}
+
+bool BM188X::FillWeightVisitor::isWritten(const onnx::Tensor* pOpnd) const
+{
+  return (m_DoneOpndSet.end() != m_DoneOpndSet.find(pOpnd));
+}
+
+void BM188X::FillWeightVisitor::setWritten(const onnx::Tensor* pOpnd)
+{
+  m_DoneOpndSet.insert(pOpnd);
 }
--- lib/Target/TG/BM188x/FillWeightVisitor.h
+++ lib/Target/TG/BM188x/FillWeightVisitor.h
@@ -9,12 +9,16 @@
 #define ONNC_TARGET_TG_BM188X_FILL_WEIGHT_VISITOR_H
 #include "BM188xVisitor.h"
 #include "GenWeightPass.h"
+#include <assert.h>
 
 namespace onnc {
 namespace BM188X {
 
 class FillWeightVisitor : public BM188xVisitor
 {
+public:
+  using Weight = GenWeightPass::WeightType;
+
 public:
   using BM188xVisitor::visit;
 
@@ -24,10 +28,28 @@ public:
 
   void visit(const BM188X::SlicedConv& pSlicedConv) override;
 
-  FillWeightVisitor(GenWeightPass::WeightType& pWeight);
+  FillWeightVisitor(Weight& pWeight);
 
 private:
-  GenWeightPass::WeightType& m_Weight;
+  /// remember the written TLConv's memory operands to prevent from
+  /// duplicatedly written.
+  typedef std::unordered_set<const onnx::Tensor*> DoneOpndSet;
+
+private:
+  static void Convert(Weight& pWeight, const std::string& pRaw,
+                      int pKS, int pIC, int pOC);
+
+  static void Append8bit(Weight& pW, const std::string &pData);
+
+  static void Append16bit(Weight& pW, const std::string &pData);
+
+  bool isWritten(const onnx::Tensor* pOpnd) const;
+
+  void setWritten(const onnx::Tensor* pOpnd);
+
+private:
+  Weight& m_Weight;
+  DoneOpndSet m_DoneOpndSet;
 };
 
 } // namespace BM188X
