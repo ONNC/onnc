2e5fc41c5dab8d43babebd4153ac2a32fce4fb0c
LuisHsu <luishsu@skymizer.com>
2018-08-13 19:39:02 +0800

    BM168x Target

--- lib/Target/cmake/proto.cmake
+++ lib/Target/cmake/proto.cmake
@@ -1,33 +1,25 @@
 
 function(gen_proto_cpp)
-    cmake_parse_arguments(OPT "" "TARGET;SOURCE;HDRS;PROTO" "" ${ARGN})
-    #prepare proto name
+    cmake_parse_arguments(OPT "" "TARGET;SOURCE;HDRS;PROTO;DEPEND" "" ${ARGN})
+    # prepare proto name
     get_filename_component(OPT_PROTO ${OPT_PROTO} ABSOLUTE)
     get_filename_component(proto_name ${OPT_PROTO} NAME)
-    get_filename_component(proto_name_we ${OPT_PROTO} NAME_WE)
     get_filename_component(proto_path ${OPT_PROTO} DIRECTORY)
-    #prepare target name and path
-    file(RELATIVE_PATH relative_path ${LIB_ONNC_DIR} ${CMAKE_CURRENT_SOURCE_DIR})
-    set(target_name libonnc_${relative_path}__${proto_name})
-    string(REPLACE "/" "_" target_name ${target_name})
-    string(REPLACE "." "_" target_name ${target_name})
-    set(${OPT_TARGET} ${target_name} PARENT_SCOPE)
-    set(out_path ${LIB_ONNC_BINARY_DIR}/include/onnc/${relative_path})
-    #set return variable
-    set(hdrs ${out_path}/${proto_name_we}.pb.h)
-    set(srcs ${out_path}/${proto_name_we}.pb.cc)
-    set(${OPT_SRCS} ${srcs} PARENT_SCOPE)
-    set(${OPT_HDRS} ${hdrs} PARENT_SCOPE)
+    get_filename_component(proto_name_we ${OPT_PROTO} NAME_WE)
+    # set return variable
+    file(RELATIVE_PATH relative_path ${ONNC_ROOT_PATH} ${CMAKE_CURRENT_SOURCE_DIR})
+    set(out_path ${PROJECT_BINARY_DIR}/include/onnc/${relative_path})
+    set(${OPT_SOURCE} "${out_path}/${proto_name_we}.pb.cc" PARENT_SCOPE)
+    set(${OPT_HDRS} "${out_path}/${proto_name_we}.pb.h" PARENT_SCOPE)
     #build
-    add_custom_target(${target_name}_dir
-        COMMAND ${CMAKE_COMMAND} -E make_directory ${out_path})
-    add_custom_command(OUTPUT ${hdrs} ${srcs}
+    add_custom_target(${OPT_TARGET}
+        COMMAND mkdir -p ${out_path}
         COMMAND protoc --proto_path=${proto_path} --cpp_out=${out_path} ${OPT_PROTO}
-        DEPENDS ${target_name}_dir ${OPT_PROTO})
-    add_custom_target(${target_name}
-        DEPENDS ${hdrs} ${srcs})
-    add_dependencies(code_gen ${target_name})
+        DEPENDS ${OPT_PROTO} ${OPT_DEPEND}
+    )
     #install
-    INSTALL(FILES ${hdrs}
-        DESTINATION include/onnc/${relative_path})
+    INSTALL(
+        FILES ${OPT_HDRS}
+        DESTINATION include/onnc/${relative_path}
+    )
 endfunction()
