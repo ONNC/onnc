f423a5a23d8e2129b3ffa4be92cba9b355281fff
LuisHsu <luishsuplus@gmail.com>
2018-08-14 19:34:30 +0800

    Sophon target

--- lib/Target/cmake/proto.cmake
+++ lib/Target/cmake/proto.cmake
@@ -1,6 +1,6 @@
 
 function(gen_proto_cpp)
-    cmake_parse_arguments(OPT "" "TARGET;SOURCE;HDRS;PROTO;DEPEND" "" ${ARGN})
+    cmake_parse_arguments(OPT "" "SOURCE;PROTO;DEPEND" "" ${ARGN})
     # prepare proto name
     get_filename_component(OPT_PROTO ${OPT_PROTO} ABSOLUTE)
     get_filename_component(proto_name ${OPT_PROTO} NAME)
@@ -8,22 +8,30 @@ function(gen_proto_cpp)
     get_filename_component(proto_name_we ${OPT_PROTO} NAME_WE)
     # set return variable
     file(RELATIVE_PATH relative_path ${ONNC_ROOT_PATH}/lib ${proto_path})
-    set(out_path ${PROJECT_BINARY_DIR}/proto/onnc/${relative_path})
-    set(${OPT_SOURCE} "${out_path}/${proto_name_we}.pb.cc" PARENT_SCOPE)
-    set(${OPT_HDRS} "${out_path}/${proto_name_we}.pb.h" PARENT_SCOPE)
-    set(${OPT_TARGET} ${OPT_TARGET} PARENT_SCOPE)
+    set(out_path ${PROJECT_BINARY_DIR}/include/onnc/${relative_path})
+    set(${OPT_SOURCE} "${out_path}/${proto_name_we}.pb.cc")
     # build
-    add_custom_target(${OPT_TARGET}
-        COMMAND mkdir -p ${out_path}
-        COMMAND protoc --proto_path=${proto_path} --cpp_out=${out_path} ${OPT_PROTO}
-        COMMAND mkdir -p ${PROJECT_BINARY_DIR}/include/onnc/${relative_path}
-        COMMAND mv ${out_path}/${proto_name_we}.pb.h ${PROJECT_BINARY_DIR}/include/onnc/${relative_path}/${proto_name_we}.pb.h
-        DEPENDS ${OPT_PROTO}
-    )
-    # depend
     if(OPT_DEPEND)
-        add_dependencies(${OPT_TARGET}
-            ${OPT_DEPEND}
+        add_custom_command(
+            OUTPUT "${out_path}/${proto_name_we}.pb.cc"
+            COMMAND mkdir -p ${out_path}
+            COMMAND protoc --proto_path=${proto_path} --cpp_out=${out_path} ${OPT_PROTO}
+            DEPENDS ${OPT_PROTO} ${OPT_DEPEND}
         )
-    endif()
+        add_library("${proto_name_we}_object" OBJECT
+            "${out_path}/${proto_name_we}.pb.cc"
+        )
+        
+    else(OPT_DEPEND)
+        add_custom_command(
+            OUTPUT "${out_path}/${proto_name_we}.pb.cc"
+            COMMAND mkdir -p ${out_path}
+            COMMAND protoc --proto_path=${proto_path} --cpp_out=${out_path} ${OPT_PROTO}
+            DEPENDS ${OPT_PROTO}
+        )
+        add_library("${proto_name_we}_object" OBJECT
+            "${out_path}/${proto_name_we}.pb.cc"
+        )
+    endif(OPT_DEPEND)
+    
 endfunction()
