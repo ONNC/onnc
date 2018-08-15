d820f1212e2baf1e892be1dd67ef455ea789c12c
LuisHsu <luishsuplus@gmail.com>
2018-08-15 14:04:33 +0800

    Fix onnx & SkyPat cmake find

--- cmake/FindSkyPat.cmake
+++ cmake/FindSkyPat.cmake
@@ -1,7 +1,9 @@
-find_library(SKYPAT_LIBRARIES
-    NAMES skypat
-)
-get_filename_component(SKYPAT_LIBRARY_DIR ${SKYPAT_LIBRARIES} DIRECTORY)
+if(NOT SKYPAT_LIBRARY_DIR)
+    find_library(SKYPAT_LIBRARIES
+        NAMES skypat
+    )
+    get_filename_component(SKYPAT_LIBRARY_DIR ${SKYPAT_LIBRARIES} DIRECTORY)
+endif(NOT SKYPAT_LIBRARY_DIR)
 find_path(SKYPAT_INCLUDE_DIR
     NAMES skypat/skypat.h
 )
--- cmake/Findonnx.cmake
+++ cmake/Findonnx.cmake
@@ -1,8 +1,8 @@
 if(NOT ONNX_LIBRARY_DIR)
-find_library(ONNX_LIBRARIES
-    NAMES onnx onnx_proto
-)
-get_filename_component(ONNX_LIBRARY_DIR ${ONNX_LIBRARIES} DIRECTORY)
+    find_library(ONNX_LIBRARIES
+        NAMES onnx onnx_proto
+    )
+    get_filename_component(ONNX_LIBRARY_DIR ${ONNX_LIBRARIES} DIRECTORY)
 endif(NOT ONNX_LIBRARY_DIR)
 find_path(ONNX_INCLUDE_DIR
     NAMES onnx/onnx.pb.h
