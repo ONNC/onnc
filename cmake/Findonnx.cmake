e513eea99d4a21eff1e9d665bb9576c5f278e4a9
LuisHsu <luishsuplus@gmail.com>
2018-08-16 14:56:57 +0800

    Replace DEFAULT_MSG to REQUIRED_VARS in "find" modules
    I misused REQUIRED_VARS with DEFAULT_MSG in FindSkyPat and Findonnx.
    
    This patch is to fix them.

--- cmake/Findonnx.cmake
+++ cmake/Findonnx.cmake
@@ -6,4 +6,4 @@ find_path(ONNX_INCLUDE_DIR
     NAMES onnx/onnx.pb.h
 )
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(ONNX DEFAULT_MSG ONNX_INCLUDE_DIR ONNX_LIBRARY_DIR)
+find_package_handle_standard_args(ONNX REQUIRED_VARS ONNX_INCLUDE_DIR ONNX_LIBRARY_DIR)
