524b00db1e7cd9852ee448226d668d23c36672ec
LuisHsu <luishsuplus@gmail.com>
2018-08-16 14:56:57 +0800

    Fix cmake find

--- cmake/FindSkyPat.cmake
+++ cmake/FindSkyPat.cmake
@@ -8,4 +8,4 @@ find_path(SKYPAT_INCLUDE_DIR
     NAMES skypat/skypat.h
 )
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(SKYPAT DEFAULT_MSG SKYPAT_INCLUDE_DIR SKYPAT_LIBRARY_DIR)
+find_package_handle_standard_args(SKYPAT REQUIRED_VARS SKYPAT_INCLUDE_DIR SKYPAT_LIBRARY_DIR)
