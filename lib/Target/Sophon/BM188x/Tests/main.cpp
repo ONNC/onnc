04ac5b3147dee674d3ab600ef3370a6794111477
LuisHsu <luishsuplus@gmail.com>
2018-08-16 17:28:22 +0800

    Fix unittest main

--- lib/Target/Sophon/BM188x/Tests/main.cpp
+++ lib/Target/Sophon/BM188x/Tests/main.cpp
@@ -10,7 +10,7 @@
 
 int main(int argc, char* argv[])
 {
-  skypat::Test::Initialize(&argc, argv);
+  skypat::Test::Initialize(argc, argv);
   skypat::Test::RunAll();
 
   return (skypat::testing::UnitTest::self()->getNumOfFails() == 0 ? EXIT_SUCCESS : EXIT_FAILURE);
