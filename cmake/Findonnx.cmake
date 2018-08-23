db9ab338f274329e3f5175325674b755e89307a4
Eric Yu <eric.yu@bitmain.com>
2018-08-23 15:30:01 +0800

    fix CMake for FindSkyPat/Findonnx.cmake

--- cmake/Findonnx.cmake
+++ cmake/Findonnx.cmake
@@ -1,9 +1,19 @@
-find_library(ONNX_LIBRARIES
-    NAMES onnx onnx_proto
-)
-get_filename_component(ONNX_LIBRARY_DIR ${ONNX_LIBRARIES} DIRECTORY)
+
+if (NOT ONNX_LIBRARIES)
+    foreach(libname onnx onnx_proto)
+        unset(libpath CACHE)
+        find_library(ONNX_LIBRARIE_${libname}
+            NAMES ${libname}
+            HINTS ${ONNX_ROOT}/lib)
+        list(APPEND ONNX_LIBRARIES ${ONNX_LIBRARIE_${libname}})
+    endforeach()
+    set(ONNX_LIBRARIES ${ONNX_LIBRARIES} CACHE STRING "")
+    get_filename_component(ONNX_LIBRARY_DIR ${ONNX_LIBRARIE_onnx} DIRECTORY)
+endif()
+
 find_path(ONNX_INCLUDE_DIR
     NAMES onnx/onnx.pb.h
+    HINTS ${ONNX_ROOT}/include
 )
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(ONNX REQUIRED_VARS ONNX_INCLUDE_DIR ONNX_LIBRARY_DIR)
