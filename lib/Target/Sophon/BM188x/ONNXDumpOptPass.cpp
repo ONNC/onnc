f1d37afa1634dc86b74a8eb08fc923a586e69f29
JerryShih <jerry.shih@iclink.tw>
2018-09-03 10:16:01 +0800

    Add "ONNXDumpQuantized" pass to export the quantized onnx model.

--- lib/Target/Sophon/BM188x/ONNXDumpOptPass.cpp
+++ lib/Target/Sophon/BM188x/ONNXDumpOptPass.cpp
@@ -34,41 +34,79 @@ private:
   BM1880Backend *m_pBackend; // NOLINT
 };
 
+class ONNXDumpQuantized : public ModulePass
+{
+public:
+  static char ID;
+
+public:
+  ONNXDumpQuantized(BM1880Backend *pBackend)
+      : ModulePass(ID), m_pBackend(pBackend)
+  {
+  }
+
+  Pass::ReturnType runOnModule(Module &pModule) override;
+
+private:
+  BM1880Backend *m_pBackend; // NOLINT
+};
+
 } // namespace
 
 char ONNXDumpOpt::ID = 0;
+char ONNXDumpQuantized::ID = 0;
 
 ModulePass *onnc::createONNXDumpOptPass(BM1880Backend *pBackend)
 {
   return new ONNXDumpOpt(pBackend);
 }
 
+static void ExportONNX(const std::string &outputFileName, const Module &pModule)
+{
+  xProto modelProto;
+  onnc::ExportModelProto(modelProto, pModule);
+  std::fstream output(outputFileName,
+                      std::ios::out | std::ios::trunc | std::ios::binary);
+  modelProto.SerializeToOstream(&output);
+}
+
 Pass::ReturnType ONNXDumpOpt::runOnModule(Module &pModule)
 {
-  // update ctable in module meta data
-  pModule.getMetaData()["bm1880_ctable"] =
-      m_pBackend->getBackendCtable().DebugString();
+  std::string outputFile = m_pBackend->options().optOnnxModel();
 
-  std::string output_file = m_pBackend->options().optOnnxModel();
-
-  if (output_file.empty()) {
+  if (outputFile.empty()) {
     errs() << "error:" << __func__ << "\n";
     exit(1);
   }
 
-  // read ctable from module and dump for debug
-  if (output_file == std::string("-")) {
-    // get ctable from module
-    std::string ctable = m_pBackend->getCtable(pModule);
-    // transfer ctable string into protobuf
-    m_pBackend->setCtableProto(ctable);
+  if (outputFile != std::string("-")) {
+    pModule.getMetaData().erase("bm1880_ctable");
+
+    ExportONNX(outputFile, pModule);
+  }
+
+  return Pass::kModuleNoChanged;
+}
+
+Pass::ReturnType ONNXDumpQuantized::runOnModule(Module &pModule)
+{
+  std::string outputFile = m_pBackend->options().optOnnxModel();
+
+  if (outputFile.empty()) {
+    errs() << "error:" << __func__ << "\n";
+    exit(1);
+  }
+
+  if (outputFile == std::string("-")) {
+    // export ctable for stdout.
     std::cout << m_pBackend->getBackendCtable().DebugString() << std::endl;
   } else {
-    xProto modelProto;
-    onnc::ExportModelProto(modelProto, pModule);
-    std::fstream output(output_file,
-                        std::ios::out | std::ios::trunc | std::ios::binary);
-    modelProto.SerializeToOstream(&output);
+    // export latest ctable
+    pModule.getMetaData()["bm1880_ctable"] =
+        m_pBackend->getBackendCtable().DebugString();
+
+    ExportONNX("quantized-" + outputFile, pModule);
   }
+
   return Pass::kModuleNoChanged;
 }
