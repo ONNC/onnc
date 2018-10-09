b54ff460c152f56801d4059e4abfb9a3a6e55cc4
Cheng-Tao Hsieh <cthsieh@gmail.com>
2018-10-09 16:36:40 +0800

    Clear vanilla backend codes.

--- lib/Target/Vanilla/TargetInfo/VanillaTargetInfo.cpp
+++ lib/Target/Vanilla/TargetInfo/VanillaTargetInfo.cpp
@@ -9,53 +9,32 @@
 #include <onnc/IR/Quadruple.h>
 #include <onnc/Target/TargetRegistry.h>
 
-///using namespace onnc;
+using namespace onnc;
 
 //===----------------------------------------------------------------------===//
 // Non-member functions
 //===----------------------------------------------------------------------===//
 
-namespace onnc {
   
-unsigned int VanillaMatchFn(const Quadruple& pQuadruple)
+static unsigned int VanillaMatchFn(const Quadruple& pQuadruple)
 {
   unsigned int score = 0;
   if (Quadruple::vanilla == pQuadruple.getArch()) {
     score += 10;
   }
 
-#if 0
-  if (Quadruple::BITMAIN == pQuadruple.getArchVendor()) {
-    score += 10;
-  }
-  if (Quadruple::BMNet == pQuadruple.getEnvironment()) {
-    score += 10;
-  }
-  if (Quadruple::SophonArchCore_TG == pQuadruple.getArchCore()) {
-    score += 30;
-  }
-  if (Quadruple::SophonSubArch_vBM1880 == pQuadruple.getSubArch()) {
-    score += 30;
-  }
-#endif
-  
   return score;
 }
 
 
-Target& getTheVanillaTarget()
+Target& onnc::getTheVanillaTarget()
 {
   static Target vanilla_target;
   return vanilla_target;
 }
 
-} // namespace onnc
-
 
 extern "C" void InitializeVanillaONNCPlatform() {
   onnc::TargetRegistry::RegisterTarget(onnc::getTheVanillaTarget(), "vanilla",
-                                       "Vanilla DLA", onnc::VanillaMatchFn);
-  
-  //onnc::RegisterTarget<onnc::Quadruple::vanilla>(onnc::getTheVanillaTarget(),
-  //    "vanilla", "Vanilla DLA");
+                                       "Vanilla DLA", VanillaMatchFn);
 }
--- lib/Target/Vanilla/TargetInfo/VanillaTargetInfo.h
+++ lib/Target/Vanilla/TargetInfo/VanillaTargetInfo.h
@@ -11,7 +11,7 @@
 
 namespace onnc {
 
-onnc::Target& getTheVanillaTarget();
+Target& getTheVanillaTarget();
 
 } // namespace of onnc
 
