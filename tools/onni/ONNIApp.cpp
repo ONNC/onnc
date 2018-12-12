46333ca5aa67523ddad6ae9e0e766aef5ba83f13
evanli <evanli@skymizer.com>
2018-12-12 15:05:07 +0000

    Use stats() instead of stat()

--- tools/onni/ONNIApp.cpp
+++ tools/onni/ONNIApp.cpp
@@ -96,7 +96,7 @@ int ONNIApp::run()
 
   if (options().verbose() >= 3) {
     errs() << "==== print CountOperatorsPass result again ====\n";
-    StatisticsGroup group = global::stat()->group("CountOperatorsPass");
+    StatisticsGroup group = global::stats()->group("CountOperatorsPass");
     StringList opList = group.entryList();
     for(auto listItr=opList.begin(); listItr != opList.end(); ++listItr){
       errs() << *listItr << ": " << group.readEntry(*listItr, 0) << std::endl;
