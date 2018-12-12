56d8bb9eae67126b79785bffca76d94058ff7a98
TDYa127 <a127a127@skymizer.com>
2018-12-12 23:35:22 +0800

    Input/Output/Initializer Use default print.

--- include/onnc/IR/Compute/InputOperator.h
+++ include/onnc/IR/Compute/InputOperator.h
@@ -52,8 +52,6 @@ public:
 
   void setTensor(Tensor& pTensor);
 
-  void print(std::ostream& pOS) const override;
-
   void accept(ComputeVisitor& pVisitor) override { pVisitor.visit(*this); }
 
   void accept(ComputeVisitor& pVisitor) const override { pVisitor.visit(*this); }
