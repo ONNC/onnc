8a8996199c6d48cacdd0cfdd2cc2276b3ea24d0c
Luba Tang <luba@skymizer.com>
2019-01-22 13:01:17 +0800

    Refine comments.

--- include/onnc/Analysis/Counter.h
+++ include/onnc/Analysis/Counter.h
@@ -27,32 +27,54 @@ namespace onnc {
  *
  *  Users can use @ref Counter::Create function to create an empty counter. Or
  *  use @ref Counter::Get to get the counter object from a well-settle group.
+ *
+ *  In case users want to claim a Counter object and then posphone assignment of
+ *  value, we also provide default constructor and assign operator for this case.
  */
 class Counter
 {
 public:
+  /// Default constructor. This create an empty group object without any value.
+  /// In this case, the counter object is invalid and users can not increase/
+  /// decrease its value.
   Counter();
 
+  /// Copy constructor. Copy the value of the counter from the others. Since
+  /// a Counter object is just an abstract interface of a json::Group object,
+  /// this copy shall not create new json::Group object; the copy operator
+  /// just create a new representation of the original json::Group object.
   Counter(const Counter& pOther);
 
+  /// Assignment. Since a Counter object is just an abstract interface of a
+  /// json::Group object, this assignment shall not create new json::Group object;
+  /// the assignment operator just create a new representation of the original
+  /// json::Group object.
   Counter& operator=(const Counter& pOther);
 
   virtual ~Counter() { /* all things in json::Object */ }
 
+  /// reset the counter to @ref pNumber.
   Counter& reset(int pNumber = 0);
 
+  /// increase one unit
   Counter& increase();
 
+  /// decrease one unit
   Counter& decrease();
 
+  /// The number of the counter
   int value() const;
 
+  /// The identity of the counter.
   StringRef name() const;
 
+  /// The description of the counter.
   StringRef getDescription() const;
 
+  /// Set description.
   void setDescription(StringRef pDesc);
 
+  /// Casting operator. Cast to the value.
   operator int() const { return value(); }
 
   Counter& operator++() { return increase(); }
@@ -66,6 +88,7 @@ public:
   void operator--(int) { decrease(); }
 
   /// If a counter wasn't given a group, it isn't active.
+  /// A Counter object created by default constructor is invalid.
   bool isActive() const;
 
   /// follow the origin value designed by evan li.
