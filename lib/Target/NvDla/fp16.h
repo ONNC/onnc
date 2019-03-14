dcc167eead2339be1338b133541b6052e9fe2d83
Po Yen Chen <poyenc@skymizer.com>
2019-03-14 13:25:19 +0800

    Fix convolution weight padding logic under image mode
    Perform weight padding when meet all of the following
    conditions:
    
        1. The convolution is first operator
        2. The number of weight channel is less than 4

--- lib/Target/NvDla/fp16.h
+++ lib/Target/NvDla/fp16.h
@@ -1,13 +1,15 @@
 #ifndef _FP16_H_
 #define _FP16_H_
 
+#include <stdbool.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 float __gnu_h2f_ieee(short param);
 short __gnu_f2h_ieee(float param);
-void  weight_pack(void* buf, float* data, int G, int dims[4], int type);
+void  weight_pack(void* buf, float* data, int G, int dims[4], int type, _Bool shouldPadZero);
 
 #ifdef __cplusplus
 }
