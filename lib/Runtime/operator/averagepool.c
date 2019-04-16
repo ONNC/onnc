5ae844fd8ea4716d0fb2a054889d70e55bdffe92
Po Yen Chen <poyenc@skymizer.com>
2019-04-16 16:02:50 +0800

    Move common operator implementation code into uitl.inc

--- lib/Runtime/operator/averagepool.c
+++ lib/Runtime/operator/averagepool.c
@@ -4,48 +4,5 @@
 #include <stdbool.h>
 #include <string.h>
 
-static inline bool next_dim(int32_t ndim, int32_t * restrict dim,
-                            const int32_t * restrict dim_max) {
-  do {
-    ndim = ndim - 1;
-    dim[ndim] += 1;
-    if (dim[ndim] < dim_max[ndim]) {
-      return true;
-    } else { // reach dimension max
-      if (ndim == 0) { // all dimension done
-        return false;
-      }
-      dim[ndim] = 0;
-    }
-  } while(true);
-}
-
-static inline int64_t dim_to_offset(int32_t ndim, const int32_t * restrict dim,
-                                    const int32_t * restrict dim_max) {
-  int64_t offset = 0;
-  int64_t step = 1;
-  for (int32_t i = ndim - 1; i >= 0; --i) {
-    offset += dim[i] * step;
-    step *= dim_max[i];
-  }
-  return offset;
-}
-
-// If it is outside the bounds of the input, use 0.
-static inline float get_value_or_zero(int32_t ndim, const int32_t * restrict dim_max,
-                                      const float * restrict value, const int32_t * restrict dim, int32_t * isPad) {
-  for (int32_t i = 0; i < ndim; ++i) {
-    if (dim[i] < 0 || dim[i] >= dim_max[i]) {
-      if(isPad){
-        *isPad = 1;
-      }
-      return 0.f;
-    }
-  }
-  if(isPad){
-    *isPad = 0;
-  }
-  return value[dim_to_offset(ndim, dim, dim_max)];
-}
-
+#include "internal/util.inc"
 #include "internal/averagepool.inc"
