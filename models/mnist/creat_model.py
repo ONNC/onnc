dfac3123ff4508523428413afd518ab375cc8e6e
sam <salmonfresh0928@gmail.com>
2019-05-26 23:33:23 +0800

    operator:conv-add

--- models/mnist/creat_model.py
+++ models/mnist/creat_model.py
@@ -18,60 +18,70 @@ Parameter6_tensor = numpy_helper.from_array(Parameter6)
 
 
 # Create one input (ValueInfoProto)
-Input3 = helper.make_tensor_value_info('Input3', TensorProto.FLOAT, [1,1,28,28])
+#Input3 = helper.make_tensor_value_info('Input3', TensorProto.FLOAT, [1,1,28,28])
 
 # Create one output (ValueInfoProto)
-Parameter5 = helper.make_tensor_value_info('Parameter5', TensorProto.FLOAT, [8,1,5,5])
+#Parameter5 = helper.make_tensor_value_info('Parameter5', TensorProto.FLOAT, [8,1,5,5])
 #Y = helper.make_tensor_value_info('Y', TensorProto.FLOAT, [1,8,28,28])
 
-Convolution28_Output_0 = helper.make_tensor_value_info('Convolution28_Output_0', TensorProto.FLOAT, [1,8,28,28])
+#Convolution28_Output_0 = helper.make_tensor_value_info('Convolution28_Output_0', TensorProto.FLOAT, [1,8,28,28])
 
 # Create one output (ValueInfoProto)
-Parameter6 = helper.make_tensor_value_info('Parameter6', TensorProto.FLOAT, [8,1,1])
-addY = helper.make_tensor_value_info('addY', TensorProto.FLOAT, [1,8,28,28])
+#Parameter6 = helper.make_tensor_value_info('Parameter6', TensorProto.FLOAT, [8,1,1])
+#addY = helper.make_tensor_value_info('addY', TensorProto.FLOAT, [1,8,28,28])
 
 
 
 # Create a node (NodeProto)
-node_def = helper.make_node(
-    'Conv', # node name
-    ['Input3','Parameter5'], # inputs
-    ['Convolution28_Output_0'], # outputs
-    'Convolution28', # node name
+#node_def = helper.make_node(
+#    'Conv', # op name
+#    ['Input3','Parameter5'], # inputs
+#    ['Convolution28_Output_0'], # outputs
+#    'Convolution28', # node name
+#    
+#    #mode='kernel_shape', # attributes
+#    kernel_shape = [5,5],
+#    auto_pad  ="SAME_UPPER",
+#    strides=[1,1],
+#    group=1,
+#    dilations = [1,1],
+#    
+#)
+#Plus30_node_def = helper.make_node(
+#    'Add', # node name
+#    ['Convolution28_Output_0','Parameter6'], # inputs
+#    ['addY'], # outputs
     
     #mode='kernel_shape', # attributes
-    kernel_shape = [5,5],
-    auto_pad  ="SAME_UPPER",
-    strides=[1,1],
-    group=1,
-    dilations = [1,1],
-    
-)
-Plus30_node_def = helper.make_node(
-    'Add', # node name
-    ['Convolution28_Output_0','Parameter6'], # inputs
-    ['addY'], # outputs
-    
-    #mode='kernel_shape', # attributes
-)
+#)
 # Create the graph (GraphProto)
 graph_def = helper.make_graph(
-    [node_def],
-    'test-model',
-    [Input3,Parameter5],
-    [Convolution28_Output_0],
-    #[Plus30_node_def],
-    #'test'
-    #[Convolution28_Output_0,Parameter6],
-    #[addY],
+    [
+        helper.make_node("Conv", ['Input3','Parameter5'], ['Convolution28_Output_0'],'Convolution28',kernel_shape = [5,5],auto_pad  ="SAME_UPPER",strides=[1,1],group=1,dilations = [1,1],),
+        helper.make_node("Add", ['Convolution28_Output_0','Parameter6'], ['Plus30_Output_0'],'Plus30'),
+        #helper.make_node("Conv", ["R1", "W2", "B2"], ["Y"]),
+    ],
+    "MLP",
+    [
+        helper.make_tensor_value_info('Input3', TensorProto.FLOAT, [1,1,28,28]),
+        helper.make_tensor_value_info('Parameter5', TensorProto.FLOAT, [8,1,5,5]),
+        #helper.make_tensor_value_info('Convolution28_Output_0', TensorProto.FLOAT, [1,8,28,28]),
+        helper.make_tensor_value_info('Parameter6', TensorProto.FLOAT, [8,1,1]),
+        #helper.make_tensor_value_info('W2', TensorProto.FLOAT, [1]),
+        #helper.make_tensor_value_info('B2', TensorProto.FLOAT, [1]),
+    ],
+    [
+        helper.make_tensor_value_info('Convolution28_Output_0', TensorProto.FLOAT, [1,8,28,28]),
+        helper.make_tensor_value_info('Plus30_Output_0', TensorProto.FLOAT, [1,8,28,28]),
+    ]
 )
 
-add_graph_def = helper.make_graph(
-    [Plus30_node_def],
-    'test-model',
-    [Convolution28_Output_0,Parameter6],
-    [addY],
-)
+#add_graph_def = helper.make_graph(
+#    [Plus30_node_def],
+#    'test-model',
+#    [Convolution28_Output_0,Parameter6],
+#    [addY],
+#)
 
 
 graph_def.initializer.extend([tensor])
@@ -79,6 +89,8 @@ graph_def.initializer.extend([tensor])
 
 graph_def.initializer[0].name = 'Parameter5'
 
+graph_def.initializer.extend([Parameter6_tensor])
+graph_def.initializer[1].name = 'Parameter6'
 #graph_def.initializer.extend([Parameter6_tensor])
 #graph_def.initializer[0].name = 'Parameter6'
 
