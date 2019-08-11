49297f1b0370e7f9968ad2ca26a07ef41cc44af6
Cheng-Tao Hsieh <cthsieh@gmail.com>
2019-08-11 16:34:49 +0800

    Clean codes of create_model.py

--- models/mnist/creat_model.py
+++ models/mnist/creat_model.py
@@ -5,80 +5,59 @@ from onnx import helper
 from onnx import AttributeProto, TensorProto, GraphProto
 
 
-
 # Preprocessing: create a Numpy array
-Parameter5 = np.load('./quantized_weight/quant_Parameter5.npy')
 # Convert the Numpy array to a TensorProto
-Parameter5_tensor = numpy_helper.from_array(Parameter5)
+# These are weights or constants for operators
+Parameter5_array = np.load('./quantized_weight/quant_Parameter5.npy')
+Parameter5_tensor = numpy_helper.from_array(Parameter5_array)
 
-Parameter6 = np.load('./quantized_weight/quant_Parameter6.npy')
+Parameter6_array = np.load('./quantized_weight/quant_Parameter6.npy')
+Parameter6_tensor = numpy_helper.from_array(Parameter6_array)
 
-Parameter6_tensor = numpy_helper.from_array(Parameter6)
+Parameter87_array = np.load('./quantized_weight/quant_Parameter87.npy')
+Parameter87_tensor = numpy_helper.from_array(Parameter87_array)
 
-Parameter87 = np.load('./quantized_weight/quant_Parameter87.npy')
-Parameter87_tensor = numpy_helper.from_array(Parameter87)
+Parameter88_array = np.load('./quantized_weight/quant_Parameter88.npy')
+Parameter88_tensor = numpy_helper.from_array(Parameter88_array)
 
+Parameter193_array = np.load('./quantized_weight/quant_Parameter193.npy')
+Parameter193_tensor = numpy_helper.from_array(Parameter193_array)
 
+Parameter194_array = np.load('./quantized_weight/quant_Parameter194.npy')
+Parameter194_tensor = numpy_helper.from_array(Parameter194_array)
 
-Parameter88 = np.load('./quantized_weight/quant_Parameter88.npy')
-Parameter88_tensor = numpy_helper.from_array(Parameter88)
+Pooling160_Output_0_reshape0_shape_array = np.load('./quantized_weight/Pooling160_Output_0_reshape0_shape.npy')
+Pooling160_Output_0_reshape0_shape_tensor = numpy_helper.from_array(Pooling160_Output_0_reshape0_shape_array)
 
+Parameter193_reshape1_shape_array = np.load('./quantized_weight/Parameter193_reshape1_shape.npy')
+Parameter193_reshape1_shape_tensor = numpy_helper.from_array(Parameter193_reshape1_shape_array)
 
+# create tensors
+Input3 =        helper.make_tensor_value_info('Input3', TensorProto.FLOAT, [1,1,28,28])
+Parameter5 =        helper.make_tensor_value_info('Parameter5', TensorProto.FLOAT, [8,1,5,5])
+Parameter6 =        helper.make_tensor_value_info('Parameter6', TensorProto.FLOAT, [8,1,1])
+Parameter87 =        helper.make_tensor_value_info('Parameter87', TensorProto.FLOAT, [16,8,5,5])
+Parameter88 =        helper.make_tensor_value_info('Parameter88', TensorProto.FLOAT, [16,1,1])
+Pooling160_Output_0_reshape0_shape =        helper.make_tensor_value_info('Pooling160_Output_0_reshape0_shape', TensorProto.FLOAT, [1,256])
+Parameter193_reshape1_shape =        helper.make_tensor_value_info('Parameter193_reshape1_shape', TensorProto.FLOAT, [256,10])
+Parameter193 =        helper.make_tensor_value_info('Parameter193', TensorProto.FLOAT, [16,4,4,10])
+Parameter194 =        helper.make_tensor_value_info('Parameter194', TensorProto.FLOAT, [1,10])
 
-
-Parameter193 = np.load('./quantized_weight/quant_Parameter193.npy')
-Parameter193_tensor = numpy_helper.from_array(Parameter193)
-
-
-
-Parameter194 = np.load('./quantized_weight/quant_Parameter194.npy')
-Parameter194_tensor = numpy_helper.from_array(Parameter194)
-
-Pooling160_Output_0_reshape0_shape = np.load('./quantized_weight/Pooling160_Output_0_reshape0_shape.npy')
-Pooling160_Output_0_reshape0_shape_tensor = numpy_helper.from_array(Pooling160_Output_0_reshape0_shape)
-
-Parameter193_reshape1_shape = np.load('./quantized_weight/Parameter193_reshape1_shape.npy')
-Parameter193_reshape1_shape_tensor = numpy_helper.from_array(Parameter193_reshape1_shape)
-
-#quant_Parameter87
-
-# Create one input (ValueInfoProto)
-#Input3 = helper.make_tensor_value_info('Input3', TensorProto.FLOAT, [1,1,28,28])
-
-# Create one output (ValueInfoProto)
-#Parameter5 = helper.make_tensor_value_info('Parameter5', TensorProto.FLOAT, [8,1,5,5])
-#Y = helper.make_tensor_value_info('Y', TensorProto.FLOAT, [1,8,28,28])
-
-#Convolution28_Output_0 = helper.make_tensor_value_info('Convolution28_Output_0', TensorProto.FLOAT, [1,8,28,28])
-
-# Create one output (ValueInfoProto)
-#Parameter6 = helper.make_tensor_value_info('Parameter6', TensorProto.FLOAT, [8,1,1])
-#addY = helper.make_tensor_value_info('addY', TensorProto.FLOAT, [1,8,28,28])
-
-
+Convolution28_Output_0 =        helper.make_tensor_value_info('Convolution28_Output_0', TensorProto.FLOAT, [1,8,28,28])
+Plus30_Output_0 =               helper.make_tensor_value_info('Plus30_Output_0', TensorProto.FLOAT, [1,8,28,28])
+ReLU32_Output_0 =               helper.make_tensor_value_info('ReLU32_Output_0', TensorProto.FLOAT, [1,8,28,28])
+Pooling66_Output_0 =            helper.make_tensor_value_info('Pooling66_Output_0', TensorProto.FLOAT, [1,8,14,14])
+Convolution110_Output_0 =       helper.make_tensor_value_info('Convolution110_Output_0', TensorProto.FLOAT, [1,16,14,14])
+Plus112_Output_0 =              helper.make_tensor_value_info('Plus112_Output_0', TensorProto.FLOAT, [1,16,14,14])
+ReLU114_Output_0 =              helper.make_tensor_value_info('ReLU114_Output_0', TensorProto.FLOAT, [1,16,14,14])
+Pooling160_Output_0 =           helper.make_tensor_value_info('Pooling160_Output_0', TensorProto.FLOAT, [1,16,4,4])
+Pooling160_Output_0_reshape0 =  helper.make_tensor_value_info('Pooling160_Output_0_reshape0', TensorProto.FLOAT, [1,256])
+Parameter193_reshape1 =         helper.make_tensor_value_info('Parameter193_reshape1', TensorProto.FLOAT, [256,10])
+Times212_Output_0 =             helper.make_tensor_value_info('Times212_Output_0', TensorProto.FLOAT, [1,10])
+Plus214_Output_0 =              helper.make_tensor_value_info('Plus214_Output_0', TensorProto.FLOAT, [1,10])
 
 # Create a node (NodeProto)
-#node_def = helper.make_node(
-#    'Conv', # op name
-#    ['Input3','Parameter5'], # inputs
-#    ['Convolution28_Output_0'], # outputs
-#    'Convolution28', # node name
-#    
-#    #mode='kernel_shape', # attributes
-#    kernel_shape = [5,5],
-#    auto_pad  ="SAME_UPPER",
-#    strides=[1,1],
-#    group=1,
-#    dilations = [1,1],
-#    
-#)
-#Plus30_node_def = helper.make_node(
-#    'Add', # node name
-#    ['Convolution28_Output_0','Parameter6'], # inputs
-#    ['addY'], # outputs
-    
-    #mode='kernel_shape', # attributes
-#)
+
 # Create the graph (GraphProto)
 graph_def = helper.make_graph(
     [
@@ -97,44 +76,23 @@ graph_def = helper.make_graph(
     ],
     "CNN",
     [
-        helper.make_tensor_value_info('Input3', TensorProto.FLOAT, [1,1,28,28]),
-        helper.make_tensor_value_info('Parameter5', TensorProto.FLOAT, [8,1,5,5]),
-        helper.make_tensor_value_info('Parameter6', TensorProto.FLOAT, [8,1,1]),
-        helper.make_tensor_value_info('Parameter87', TensorProto.FLOAT, [16,8,5,5]),
-        helper.make_tensor_value_info('Parameter88', TensorProto.FLOAT, [16,1,1]),
-        helper.make_tensor_value_info('Pooling160_Output_0_reshape0_shape', TensorProto.FLOAT, [1,256]),
-        helper.make_tensor_value_info('Parameter193_reshape1_shape', TensorProto.FLOAT, [256,10]),
-        helper.make_tensor_value_info('Parameter193', TensorProto.FLOAT, [16,4,4,10]),
-        helper.make_tensor_value_info('Parameter194', TensorProto.FLOAT, [1,10]),
-       
+        Input3,
+        Parameter5,
+        Parameter6,
+        Parameter87,
+        Parameter88,
+        Pooling160_Output_0_reshape0_shape,
+        Parameter193_reshape1_shape,
+        Parameter193,
+        Parameter194
     ],
     [
-        helper.make_tensor_value_info('Convolution28_Output_0', TensorProto.FLOAT, [1,8,28,28]),
-        helper.make_tensor_value_info('Plus30_Output_0', TensorProto.FLOAT, [1,8,28,28]),
-        helper.make_tensor_value_info('ReLU32_Output_0', TensorProto.FLOAT, [1,8,28,28]),
-        helper.make_tensor_value_info('Pooling66_Output_0', TensorProto.FLOAT, [1,8,14,14]),
-        helper.make_tensor_value_info('Convolution110_Output_0', TensorProto.FLOAT, [1,16,14,14]),
-        helper.make_tensor_value_info('Plus112_Output_0', TensorProto.FLOAT, [1,16,14,14]),
-        helper.make_tensor_value_info('ReLU114_Output_0', TensorProto.FLOAT, [1,16,14,14]),
-        helper.make_tensor_value_info('Pooling160_Output_0', TensorProto.FLOAT, [1,16,4,4]),
-        helper.make_tensor_value_info('Pooling160_Output_0_reshape0', TensorProto.FLOAT, [1,256]),
-        helper.make_tensor_value_info('Parameter193_reshape1', TensorProto.FLOAT, [256,10]),
-        helper.make_tensor_value_info('Times212_Output_0', TensorProto.FLOAT, [1,10]),
-        helper.make_tensor_value_info('Plus214_Output_0', TensorProto.FLOAT, [1,10]),
-        
+        Plus214_Output_0
     ]
 )
 
-#add_graph_def = helper.make_graph(
-#    [Plus30_node_def],
-#    'test-model',
-#    [Convolution28_Output_0,Parameter6],
-#    [addY],
-#)
-
+# set up Initializer
 graph_def.initializer.extend([Parameter5_tensor])
-#graph_def.initializer[0].name = 'no-exist'
-
 graph_def.initializer[0].name = 'Parameter5'
 
 graph_def.initializer.extend([Parameter6_tensor])
@@ -146,25 +104,11 @@ graph_def.initializer[2].name = 'Parameter87'
 graph_def.initializer.extend([Parameter88_tensor])
 graph_def.initializer[3].name = 'Parameter88'
 
-
 graph_def.initializer.extend([Pooling160_Output_0_reshape0_shape_tensor])
-#graph_def.initializer[0].name = 'no-exist'
-
 graph_def.initializer[4].name = 'Pooling160_Output_0_reshape0_shape'
-#Pooling160_Output_0_reshape0_shape = np.array([1,256])
-#Pooling160_Output_0_reshape0_shape_tensor = numpy_helper.from_array(Pooling160_Output_0_reshape0_shape)
-#graph_def.initializer.extend([Pooling160_Output_0_reshape0_shape_tensor])
-#graph_def.initializer[4].name = 'Pooling160_Output_0_reshape0_shape'
 
 graph_def.initializer.extend([Parameter193_reshape1_shape_tensor])
-#graph_def.initializer[0].name = 'no-exist'
-
 graph_def.initializer[5].name = 'Parameter193_reshape1_shape'
-#Parameter193_reshape1_shape = np.array([256,10])
-#Parameter193_reshape1_shape_tensor = numpy_helper.from_array(Parameter193_reshape1_shape)
-#graph_def.initializer.extend([Parameter193_reshape1_shape_tensor])
-#graph_def.initializer[5].name = 'Parameter193_reshape1_shape'
-
 
 graph_def.initializer.extend([Parameter193_tensor])
 graph_def.initializer[6].name = 'Parameter193'
@@ -182,4 +126,4 @@ onnx.checker.check_model(model_def)
 #converted_model = version_converter.convert_version(model_def, <int target_version>)
 print('The model is checked!')
 #print(graph_def.initializer)
-onnx.save(model_def, './quantized.onnx')
+onnx.save(model_def, './quantized-new.onnx')
