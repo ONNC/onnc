aa517a28a671362ef60b42f7714bf12b85d25ffe
albert0liu0 <b04902012@ntu.edu.tw>
2019-08-26 14:34:37 +0800

    modified cmakelists

--- export/runtime/include/make_library.sh
+++
@@ -1,1 +0,0 @@
-gcc -c -o operators.o operators.c -lm
--- export/runtime/make_inference.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p build
-cd build/
-sudo cmake ../
-sudo make
-cd ..
-cd include/
-./make_library.sh
-cd ..
-sudo gcc -o build/example/inference build/example/libinference.a include/operators.o -lm
