9d69684996ab5e2f5aaea391017beff653662ec4
gisiu <gisiu@skymizer.com>
2019-09-25 06:22:51 +0000

    [backend][cortexm] Remove unused files
    These removed files are not used here

--- lib/Target/CortexM/add.h
+++
@@ -1,9 +0,0 @@
-#ifndef ADD_H
-#define ADD_H
-
-#include "arm_math.h"
-
-void MatAdd(q7_t* input, int* input_dim, q7_t* add, int* add_dim, q7_t* output,
-            int number_of_input_dim, int right_shift, int add_shift);
-
-#endif
