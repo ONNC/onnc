eabc007ed61621da37b200aa40a0bcf61afc94b7
gisiu <gisiu@skymizer.com>
2019-09-25 10:51:48 +0000

    [backend][cortexm] Remove unused files

--- cortexm_out.h
+++
@@ -1,9 +0,0 @@
-#ifndef CORTEXM_OUT_H
-#define CORTEXM_OUT_H
-
-#include <stdio.h>
-#include <stdlib.h>
-#include "arm_math.h"
-q7_t* cortexm_main(int* image_data);
-
-#endif
--- cortexm_weight.h
+++
@@ -1,3 +0,0 @@
-#define RIGHT_SHIFT1 0
-#define CONV1_WT {1,1,1,1,1,1,1,1,1}
-#define CONV1_BIAS {0}
