d8678f5a4797d45783d7f1d0709b5a1470809393
gisiu <gisiu@skymizer.com>
2019-09-27 05:38:39 +0000

    [backend][cortexm] Apply latest clang-formate under lib/Target/CortexM/tests

--- lib/Target/CortexM/tests/MatAdd_2D/input_data.h
+++ lib/Target/CortexM/tests/MatAdd_2D/input_data.h
@@ -1,13 +1,13 @@
 #include "arm_math.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 
 typedef int8_t q7_t;
-q7_t inputA[36] = {
-    1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0,
-    1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0,
+q7_t           inputA[36] = {
+  1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0,
 };
-q7_t inputB[36] = {2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+q7_t  inputB[36]    = {2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
                    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2};
-q7_t *inputA_buffer = inputA;
-q7_t *inputB_buffer = inputB;
+q7_t* inputA_buffer = inputA;
+q7_t* inputB_buffer = inputB;
